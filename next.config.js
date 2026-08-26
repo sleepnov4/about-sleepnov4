2cc63c39241fb62897ccbbaa974ff2ecaa55c6aa
Catharsis <frostgamers.fx@gmail.com>
2026-08-26 22:52:33 +0700

    refactor(maintenance): removed unused files for maintenance page

--- next.config.js
+++
@@ -1,9 +0,0 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
-
-// module.exports = nextConfig
-
-const nextra = require('nextra')('nextra-theme-blog', './theme.config.js')
-module.exports = nextra()
